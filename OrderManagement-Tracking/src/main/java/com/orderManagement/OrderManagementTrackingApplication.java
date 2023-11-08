d57ddb80330b371d06075bd2c2d78321916a4574
aswintomy <aswintomy09@gmail.com>
2023-11-08 19:04:47 +0530

    initial commit

---
+++ OrderManagement-Tracking/src/main/java/com/orderManagement/OrderManagementTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.orderManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementTrackingApplication.class, args);
+	}
+
+}
---
+++ OrderManagement-Tracking/src/main/java/com/orderManagement/service/OrderCaptureService.java
@@ -0,0 +1,5 @@
+package com.orderManagement.service;
+
+public interface OrderCaptureService {
+
+}
